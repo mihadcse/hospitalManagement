aeac1dd2e6d98784186c1eed50dcf2eb8e8424e5
iftiAkkas <iftiakkas@iut-dhaka.edu>
2025-10-28 12:54:11 +0600

    image added

--- backendHMS/src/main/java/com/healthcare/demo/dto/PatientDto.java
+++ backendHMS/src/main/java/com/healthcare/demo/dto/PatientDto.java
@@ -12,4 +12,5 @@ public class PatientDto {
     private String name;
     private String email;
     private String phone;
+    private String imageUrl; // 🆕
 }
--- backendHMS/src/main/java/com/healthcare/demo/models/Patient.java
+++ backendHMS/src/main/java/com/healthcare/demo/models/Patient.java
@@ -3,7 +3,6 @@ package com.healthcare.demo.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.List;
 
 @Entity
@@ -16,14 +15,13 @@ public class Patient {
     private Long id;
 
     private String name;
-
     private String email;
-
     private String phone;
-
     private String password;
 
-    // Relation with Appointment
+    // 🆕 Add image URL field
+    private String imageUrl;
+
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Appointment> appointments;
 }
